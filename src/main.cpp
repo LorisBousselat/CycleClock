5f1703b1aa974081020ef857a776e29ad60b2fcb
Loris <loris.bousselat@hotmail.com>
2024-10-18 11:27:22 -0400

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+
+///////////////////////////////////////////////////////////////////////////
+// Project : CycleClock
+// Author : Loris Bousselat
+// Date : 2024-October-18
+// Equipment used : eps32    7-segment Display with 74HC595
+// Description :
+//   Make a segment run arround the display
+///////////////////////////////////////////////////////////////////////////
+
+const int STcp = 27;//ST_CP
+const int SHcp = 26;//SH_CP 
+const int DS = 25; //DS 
+
+int datArray[] = {B00000001, B00000010, B00000100, B00001000, B00010000, B00100000};
+
+
+void setup() {
+  //set pins to output
+  pinMode(STcp,OUTPUT);
+  pinMode(SHcp,OUTPUT);
+  pinMode(DS,OUTPUT);
+  
+}
+
+void loop() {
+  for(int num = 0; num <sizeof(datArray)/sizeof(datArray[0]); num++)
+  {
+    digitalWrite(STcp,LOW); //ground ST_CP and hold low for as long as you are transmitting
+    shiftOut(DS,SHcp,MSBFIRST,datArray[num]);
+    digitalWrite(STcp,HIGH); //pull the ST_CPST_CP to save the data
+    delay(100);
+  }
+}
