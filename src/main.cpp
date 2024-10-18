571898d04095e04a511abdd4609b2456f19fb655
Loris <loris.bousselat@hotmail.com>
2024-10-18 13:09:49 -0400

    Changes to be committed: modified:   src/main.cpp -> sizeOf

--- src/main.cpp
+++ src/main.cpp
@@ -9,27 +9,28 @@
 //   Make a segment run arround the display
 ///////////////////////////////////////////////////////////////////////////
 
-const int STcp = 27;//ST_CP
-const int SHcp = 26;//SH_CP 
-const int DS = 25; //DS 
+const int STcp = 27; // ST_CP
+const int SHcp = 26; // SH_CP
+const int DS = 25;   // DS
 
 int datArray[] = {B00000001, B00000010, B00000100, B00001000, B00010000, B00100000};
+int datSize = sizeof(datArray) / sizeof(datArray[0]);
 
-
-void setup() {
-  //set pins to output
-  pinMode(STcp,OUTPUT);
-  pinMode(SHcp,OUTPUT);
-  pinMode(DS,OUTPUT);
-  
+void setup()
+{
+  // set pins to output
+  pinMode(STcp, OUTPUT);
+  pinMode(SHcp, OUTPUT);
+  pinMode(DS, OUTPUT);
 }
 
-void loop() {
-  for(int num = 0; num <sizeof(datArray)/sizeof(datArray[0]); num++)
+void loop()
+{
+  for (int i = 0; i < datSize; i++)
   {
-    digitalWrite(STcp,LOW); //ground ST_CP and hold low for as long as you are transmitting
-    shiftOut(DS,SHcp,MSBFIRST,datArray[num]);
-    digitalWrite(STcp,HIGH); //pull the ST_CPST_CP to save the data
+    digitalWrite(STcp, LOW); // ground ST_CP and hold low for as long as you are transmitting
+    shiftOut(DS, SHcp, MSBFIRST, datArray[i]);
+    digitalWrite(STcp, HIGH); // pull the ST_CPST_CP to save the data
     delay(100);
   }
 }
